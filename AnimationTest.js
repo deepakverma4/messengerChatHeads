105b24c95224c8185e0dc0d125c54909959e48b3
deepakverma4 <er.deepakverma4u@gmail.com>
2021-09-22 17:50:55 +0530

    chore: added animated button progress

--- AnimationTest.js
+++ AnimationTest.js
@@ -10,6 +10,7 @@ import {
   PanResponder,
   Animated,
   Image,
+  TouchableOpacity,
 } from 'react-native';
 // import  from 'react-native-reanimated';
 import clamp from 'clamp';
@@ -19,201 +20,67 @@ export class AnimationTest extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      animation: new Animated.ValueXY(),
+      animation: new Animated.Value(0),
       opacity: new Animated.Value(1),
-      next: new Animated.Value(0.9),
-      data: [
-        {
-          color: 'tomato',
-          animation: new Animated.ValueXY(),
-          image:
-            'https://e7.pngegg.com/pngimages/614/838/png-clipart-cute-cat-animal-cat.png',
-        },
-        {
-          color: 'blue',
-          animation: new Animated.ValueXY(),
-          image:
-            'https://spng.pngfind.com/pngs/s/123-1234419_free-png-download-cute-cat-png-images-background.png',
-        },
-        {
-          color: 'green',
-          animation: new Animated.ValueXY(),
-          image:
-            'https://purepng.com/public/uploads/large/51502305566rtsfses9licuhzwzwc7hwh72e9boqm9vseph08hvvkhxgljuvmepx0w1xzaphsjwx0aftsgcwjfww6pmfytkr5pb6er0mmwugqnp.png',
-        },
-        {
-          color: 'aqua',
-          animation: new Animated.ValueXY(),
-          image:
-            'https://png.pngtree.com/png-clipart/20210309/original/pngtree-an-adult-tabby-cat-png-image_5803657.jpg',
-        },
-        {
-          color: 'yellow',
-          animation: new Animated.ValueXY(),
-          image:
-            'https://files.oyebesmartest.com/uploads/preview/cat-png505wgf.png',
-        },
-      ],
     };
-    this._panResponder = PanResponder.create({
-      onStartShouldSetPanResponder: () => true,
-      onMoveShouldSetPanResponder: () => true,
-      onPanResponderMove: (e, {dx, dy}) => {
-        this.state.animation.setValue({
-          x: dx,
-          y: dy,
-          useNativeDriver: true,
-        });
-      },
-      onPanResponderGrant: () => {
-        this.state.animation.extractOffset();
-      },
-      onPanResponderRelease: (e, {dx, vx, vy}) => {
-        let velocity;
-
-        if (vx >= 0) {
-          velocity = clamp(vx, 3, 5);
-        } else if (vx < 0) {
-          velocity = clamp(Math.abs(vx), 3, 5) * -1;
-        }
-
-        if (Math.abs(dx) > SWIPE_THRESHOLD) {
-          Animated.decay(this.state.animation, {
-            velocity: {x: velocity, y: vy},
-            deceleration: 0.98,
-            useNativeDriver: true,
-          }).start(() => {
-            this.goToNext();
-          });
-        } else {
-          Animated.spring(this.state.animation, {
-            toValue: {x: 0, y: 0},
-            friction: 20,
-            useNativeDriver: true,
-          }).start();
-        }
-      },
-    });
   }
 
-  goToNext = () => {
-    Animated.parallel([
+  startAnimation = () => {
+    this.state.animation.setValue(0);
+    this.state.opacity.setValue(1);
+    Animated.timing(this.state.animation, {
+      toValue: 1,
+      duration: 1500,
+      useNativeDriver: false,
+    }).start(() => {
       Animated.timing(this.state.opacity, {
-        toValue: 1,
-        duration: 30,
-        useNativeDriver: true,
-      }),
-      Animated.spring(this.state.next, {
-        toValue: 1,
-        friction: 4,
-        useNativeDriver: true,
-      }),
-    ]).start(() => {
-      this.setState(
-        (state) => {
-          return {
-            data: state.data.slice(1),
-          };
-        },
-        () => {
-          this.state.animation.setValue({x: 0, y: 0});
-          this.state.opacity.setValue(1);
-          this.state.next.setValue(0.9);
-        },
-      );
+        toValue: 0,
+        duration: 1500,
+        useNativeDriver: false,
+      }).start();
     });
   };
 
-  componentDidMount() {}
-
   render() {
-    const topInterpolate = this.state.animation.x.interpolate({
-      inputRange: [-250, 0, 250],
-      outputRange: ['-30deg', '0deg', '30deg'],
-      extrapolate: 'clamp',
+    const widthInterpolate = this.state.animation.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0%', '100%'],
+      // extrapolate: 'clamp',
     });
 
-    // const scaleInterpolate = this.state.animation.x.interpolate({
-    //   inputRange: [-250, 0, 250],
-    //   outputRange: [0.8, 1, 0.5],
-    //   extrapolate: 'clamp'
-    // });
-    const opacity = this.state.animation.x.interpolate({
-      inputRange: [-250, 0, 250],
-      outputRange: [0.5, 1, 0.5],
-      extrapolate: 'clamp',
+    const colorInterpolate = this.state.animation.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['#22bb33', '#22bb33'],
+      // extrapolate: 'clamp',
     });
 
-    const newStyle = {
-      transform: [
-        {rotate: topInterpolate},
-
-        ...this.state.animation.getTranslateTransform(),
-      ],
+    const animatedStyle = {
+      backgroundColor: colorInterpolate,
+      width: widthInterpolate,
       opacity: this.state.opacity,
     };
+
     return (
       <SafeAreaView style={styles.container}>
-        {this.state.data
-          .slice(0)
-          .reverse()
-          .map(({animation, color, image}, index) => {
-            const isSecondLast = index === this.state.data.length - 2;
-            const isLast = index === this.state.data.length - 1;
-            const panHandlers = isLast ? this._panResponder.panHandlers : {};
-            const animatedStyle = isLast ? newStyle : {};
-            const nextStyle = isSecondLast
-              ? {transform: [{scale: this.state.next}]}
-              : {};
-            return (
-              <Animated.View
-                key={index}
-                style={[
-                  {
-                    height: 300,
-                    width: 300,
-                    shadowColor: '#000',
-                    shadowOffset: {height: 0, width: 0},
-                    shadowOpacity: 0.3,
-                    shadowRadius: 5,
-                    elevation: 2,
-                    position: 'absolute',
-                    backgroundColor: 'white',
-                    borderRadius: 5,
-                  },
-                  animatedStyle,
-                ]}
-                {...panHandlers}>
-                <Animated.Image
-                  style={[
-                    {
-                      height: null,
-                      width: null,
-                      flex: 3,
-                      borderRadius: 5,
-
-                      backgroundColor: 'white',
-                    },
-                    nextStyle,
-                  ]}
-                  source={{uri: image}}
-                />
-                <View
-                  style={{
-                    flex: 1,
-                    borderRadius: 5,
-                    backgroundColor: 'white',
-                    position: 'absolute',
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                  }}>
-                  <Text
-                    style={{fontSize: 18, padding: 5}}>{`Hello ${index}`}</Text>
-                </View>
-              </Animated.View>
-            );
-          })}
+        <TouchableOpacity
+          onPress={this.startAnimation}
+          style={{
+            height: 50,
+            width: '60%',
+            backgroundColor: 'tomato',
+            borderRadius: 5,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Animated.View
+            style={[
+              StyleSheet.absoluteFill,
+              // {backgroundColor: 'red'},
+              animatedStyle,
+            ]}
+          />
+          <Text style={[{fontSize: 18, color: '#fff'}]}>Press me</Text>
+        </TouchableOpacity>
       </SafeAreaView>
     );
   }
