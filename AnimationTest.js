684d910d232bfa64f43a79a98d1b8d0fa3b32a4b
deepakverma4 <er.deepakverma4u@gmail.com>
2021-09-23 19:08:52 +0530

    chore: animated color picker view

--- AnimationTest.js
+++ AnimationTest.js
@@ -11,290 +11,225 @@ import {
   Animated,
   Image,
   TouchableOpacity,
+  TextInput,
 } from 'react-native';
 // import  from 'react-native-reanimated';
 
+const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
 export class AnimationTest extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: [
-        {
-          color: 'tomato',
-
-          image:
-            'https://e7.pngegg.com/pngimages/614/838/png-clipart-cute-cat-animal-cat.png',
-        },
-        {
-          color: 'blue',
-
-          image:
-            'https://spng.pngfind.com/pngs/s/123-1234419_free-png-download-cute-cat-png-images-background.png',
-        },
-        {
-          color: 'green',
-
-          image:
-            'https://purepng.com/public/uploads/large/51502305566rtsfses9licuhzwzwc7hwh72e9boqm9vseph08hvvkhxgljuvmepx0w1xzaphsjwx0aftsgcwjfww6pmfytkr5pb6er0mmwugqnp.png',
-        },
-        {
-          color: 'aqua',
-
-          image:
-            'https://png.pngtree.com/png-clipart/20210309/original/pngtree-an-adult-tabby-cat-png-image_5803657.jpg',
-        },
-        {
-          color: 'yellow',
-
-          image:
-            'https://files.oyebesmartest.com/uploads/preview/cat-png505wgf.png',
-        },
-        {
-          color: 'blue',
-
-          image:
-            'https://spng.pngfind.com/pngs/s/123-1234419_free-png-download-cute-cat-png-images-background.png',
-        },
-        {
-          color: 'green',
-
-          image:
-            'https://purepng.com/public/uploads/large/51502305566rtsfses9licuhzwzwc7hwh72e9boqm9vseph08hvvkhxgljuvmepx0w1xzaphsjwx0aftsgcwjfww6pmfytkr5pb6er0mmwugqnp.png',
-        },
-        {
-          color: 'aqua',
-
-          image:
-            'https://png.pngtree.com/png-clipart/20210309/original/pngtree-an-adult-tabby-cat-png-image_5803657.jpg',
-        },
-        {
-          color: 'yellow',
-
-          image:
-            'https://files.oyebesmartest.com/uploads/preview/cat-png505wgf.png',
-        },
-        {
-          color: 'blue',
-
-          image:
-            'https://spng.pngfind.com/pngs/s/123-1234419_free-png-download-cute-cat-png-images-background.png',
-        },
-        {
-          color: 'green',
-
-          image:
-            'https://purepng.com/public/uploads/large/51502305566rtsfses9licuhzwzwc7hwh72e9boqm9vseph08hvvkhxgljuvmepx0w1xzaphsjwx0aftsgcwjfww6pmfytkr5pb6er0mmwugqnp.png',
-        },
-        {
-          color: 'aqua',
-
-          image:
-            'https://png.pngtree.com/png-clipart/20210309/original/pngtree-an-adult-tabby-cat-png-image_5803657.jpg',
-        },
-        {
-          color: 'yellow',
-
-          image:
-            'https://files.oyebesmartest.com/uploads/preview/cat-png505wgf.png',
-        },
-        {
-          color: 'blue',
-
-          image:
-            'https://spng.pngfind.com/pngs/s/123-1234419_free-png-download-cute-cat-png-images-background.png',
-        },
-        {
-          color: 'green',
-
-          image:
-            'https://purepng.com/public/uploads/large/51502305566rtsfses9licuhzwzwc7hwh72e9boqm9vseph08hvvkhxgljuvmepx0w1xzaphsjwx0aftsgcwjfww6pmfytkr5pb6er0mmwugqnp.png',
-        },
-      ],
-      position: new Animated.ValueXY(),
-      size: new Animated.ValueXY(),
-      animation: new Animated.Value(0),
-      activeImage: null,
-      // activeIndex: null,
+      viewAnimation: new Animated.Value(0),
+      buttonAnimation: new Animated.Value(0),
+      showView: true,
+      showButton: true,
+      color: '#333',
     };
-
-    this._images = {};
   }
 
-  onImagePress = (index) => {
-    this._images[index].measure((x, y, width, height, pageX, pageY) => {
-      console.log(
-        x,
-        y,
-        pageX,
-        pageY,
-        height,
-        width,
-        'x,y,pageX,pageY,height,width',
-      );
+  toggleView = () => {
+    const toValue = this.state.showView ? 1 : 0;
+    Animated.spring(this.state.viewAnimation, {
+      toValue,
+      useNativeDriver: true,
+    }).start();
 
-      this._x = pageX;
-      this._y = pageY;
-      this._height = height;
-      this._width = width;
-
-      this.state.position.setValue({x: pageX, y: pageY});
-      this.state.size.setValue({x: width, y: height});
-
-      this.setState(
-        {
-          activeImage: this.state.data[index].image,
-          activeIndex: index,
-        },
-        () => {
-          this.viewImage.measure((tx, ty, twidth, theight, tpageX, tpageY) => {
-            Animated.parallel([
-              Animated.spring(this.state.position.x, {
-                toValue: tpageX,
-                useNativeDriver: false,
-              }),
-              Animated.spring(this.state.position.y, {
-                toValue: tpageY,
-                useNativeDriver: false,
-              }),
-              Animated.spring(this.state.size.x, {
-                toValue: twidth,
-                useNativeDriver: false,
-              }),
-              Animated.spring(this.state.size.y, {
-                toValue: theight,
-                useNativeDriver: false,
-              }),
-              Animated.spring(this.state.animation, {
-                toValue: 1,
-                useNativeDriver: false,
-              }),
-            ]).start();
-          });
-        },
-      );
+    this.setState({
+      showView: !this.state.showView,
     });
   };
 
-  closePressed = () => {
-    Animated.parallel([
-      Animated.timing(this.state.position.x, {
-        toValue: this._x,
-        duration: 250,
-        useNativeDriver: false,
-      }),
-      Animated.timing(this.state.position.y, {
-        toValue: this._y,
-        duration: 250,
-        useNativeDriver: false,
-      }),
-      Animated.timing(this.state.size.x, {
-        toValue: this._width,
-        duration: 250,
-        useNativeDriver: false,
-      }),
-      Animated.timing(this.state.size.y, {
-        toValue: this._height,
-        duration: 250,
-        useNativeDriver: false,
-      }),
-      Animated.timing(this.state.animation, {
+  toggleButton = () => {
+    const toValue = this.state.showButton ? 1 : 0;
+    Animated.timing(this.state.buttonAnimation, {
+      toValue,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+
+    this.setState({
+      showButton: !this.state.showButton,
+    });
+  };
+
+  colorChange = (text) => {
+    this.setState({
+      color: text,
+    });
+  };
+
+  closeButton = () => {
+    Animated.sequence([
+      Animated.timing(this.state.buttonAnimation, {
         toValue: 0,
-        duration: 250,
-        useNativeDriver: false,
+        duration: 300,
+        useNativeDriver: true,
+      }),
+      Animated.delay(10),
+      Animated.spring(this.state.viewAnimation, {
+        toValue: 0,
+        useNativeDriver: true,
       }),
     ]).start(() => {
       this.setState({
-        activeImage: null,
-        // activeIndex: 0,
+        showView: false,
+        showButton: false,
       });
     });
   };
-
   render() {
-    console.log(this.state.activeImage, 'this.state.activeImage');
-    const translateInterpolate = this.state.animation.interpolate({
+    const viewInterpolate = this.state.viewAnimation.interpolate({
       inputRange: [0, 1],
-      outputRange: [300, 0],
+      outputRange: [0, -100],
     });
 
-    const viewStyle = {
-      opacity: this.state.animation,
-      transform: [{translateY: translateInterpolate}],
+    const viewScaleInterpolate = this.state.viewAnimation.interpolate({
+      inputRange: [0, 0.5, 1],
+      outputRange: [0, 0, 1],
+    });
+
+    const animatedViewStyle = {
+      opacity: this.state.viewAnimation,
+      transform: [
+        {
+          translateY: viewInterpolate,
+        },
+        {scaleX: viewScaleInterpolate},
+      ],
     };
 
-    const activeImageStyle = {
-      width: this.state.size.x,
-      height: this.state.size.y,
-      top: this.state.position.y,
-      left: this.state.position.x,
+    const moveInterpolate = this.state.buttonAnimation.interpolate({
+      inputRange: [0, 1],
+      outputRange: [-50, 0],
+    });
+
+    const viewInterpolateOpacity = this.state.buttonAnimation.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0, 1],
+    });
+
+    const textInterpolateOpacity = this.state.buttonAnimation.interpolate({
+      inputRange: [0, 0.5, 1],
+      outputRange: [1, 1, 0],
+    });
+
+    const animatedTextStyle = {
+      opacity: textInterpolateOpacity,
+      transform: [{scale: textInterpolateOpacity}],
     };
 
-    const activeIndexStyle = {
-      opacity: this.state.activeImage ? 0 : 1,
+    const animatedTopView = {
+      transform: [
+        {translateX: moveInterpolate},
+        {scaleY: this.state.buttonAnimation},
+      ],
+      opacity: viewInterpolateOpacity,
     };
 
-    const animatedButtonStyle = {
-      opacity: this.state.activeImage ? 1 : 0,
-    };
     return (
       <SafeAreaView style={styles.container}>
-        <ScrollView
-          style={styles.container}
-          contentContainerStyle={{
-            // flex: 1,
-            flexDirection: 'row',
-            flexWrap: 'wrap',
-          }}>
-          {this.state.data.map(({image}, index) => {
-            const newStyle =
-              image === this.state.activeIndex ? activeIndexStyle : {};
-            return (
-              <TouchableWithoutFeedback
-                key={index}
-                onPress={() => {
-                  this.onImagePress(index);
-                }}>
-                <Animated.Image
-                  key={index}
-                  ref={(ref) => (this._images[index] = ref)}
-                  style={[styles.image, newStyle]}
-                  resizeMode="cover"
-                  source={{uri: image}}
-                />
-              </TouchableWithoutFeedback>
-            );
-          })}
-        </ScrollView>
-
-        <View
-          style={StyleSheet.absoluteFill}
-          pointerEvents={this.state.activeImage ? 'auto' : 'none'}>
-          <View style={{flex: 1}} ref={(view) => (this.viewImage = view)}>
-            <Animated.Image
-              key={this.state.activeImage}
-              style={[styles.largeImage, activeImageStyle]}
-              resizeMode="cover"
-              source={{uri: this.state.activeImage}}
+        <Animated.View style={[styles.viewStyle, animatedViewStyle]}>
+          <TouchableOpacity onPress={this.toggleButton}>
+            <View
+              style={{
+                height: 20,
+                width: 20,
+                backgroundColor: this.state.color,
+                borderRadius: 20,
+              }}
             />
+          </TouchableOpacity>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-around',
+
+              flex: 1,
+            }}>
+            <TouchableOpacity
+              style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                padding: 3,
+              }}>
+              <Animated.Text style={[{fontSize: 20}, animatedTextStyle]}>
+                A
+              </Animated.Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                padding: 3,
+              }}>
+              <Animated.Text style={[{fontSize: 20}, animatedTextStyle]}>
+                B
+              </Animated.Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                padding: 3,
+              }}>
+              <Animated.Text style={[{fontSize: 20}, animatedTextStyle]}>
+                C
+              </Animated.Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                padding: 3,
+              }}>
+              <Animated.Text style={[{fontSize: 20}, animatedTextStyle]}>
+                D
+              </Animated.Text>
+            </TouchableOpacity>
           </View>
+
           <Animated.View
-            style={[{backgroundColor: 'white', flex: 2}, viewStyle]}>
-            <Text style={{fontSize: 16, padding: 5}}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </Text>
+            pointerEvents={this.state.showButton ? 'none' : 'auto'}
+            style={[
+              StyleSheet.absoluteFill,
+              {
+                flexDirection: 'row',
+                alignItems: 'center',
+                paddingLeft: 30,
+              },
+              animatedTopView,
+            ]}>
+            <AnimatedTextInput
+              ref={(input) => (this._input = input)}
+              style={{flex: 1}}
+              value={this.state.color}
+              onChangeText={this.colorChange}
+            />
+            <TouchableOpacity onPress={this.closeButton}>
+              <Animated.View style={[styles.buttonStyle]}>
+                <Text style={{color: '#fff', padding: 5}}>OK</Text>
+              </Animated.View>
+            </TouchableOpacity>
           </Animated.View>
-        </View>
-        <TouchableWithoutFeedback onPress={this.closePressed}>
-          <Animated.View style={[styles.buttonStyle, animatedButtonStyle]}>
-            <Text style={[styles.textStyle]}>X</Text>
-          </Animated.View>
-        </TouchableWithoutFeedback>
+        </Animated.View>
+        <TouchableOpacity
+          activeOpacity={0.6}
+          onPress={this.toggleView}
+          style={{
+            borderRadius: 10,
+            borderWidth: 0.8,
+            borderColor: '#333',
+            paddingVertical: 5,
+            paddingHorizontal: 8,
+          }}>
+          <Text style={[styles.textStyle, {color: this.state.color}]}>
+            Toggle View
+          </Text>
+        </TouchableOpacity>
       </SafeAreaView>
     );
   }
@@ -308,29 +243,32 @@ const styles = StyleSheet.create({
     // flexGrow: 1,
 
     backgroundColor: 'white',
-  },
-  image: {
-    width: '33%',
-    height: 140,
-    backgroundColor: 'white',
-  },
-  largeImage: {
-    width: null,
-    height: null,
-    position: 'absolute',
-    left: 0,
-    top: 0,
-    // backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   textStyle: {
-    fontSize: 30,
-    color: 'black',
+    fontSize: 18,
     padding: 5,
-    fontWeight: 'bold',
+  },
+  viewStyle: {
+    backgroundColor: '#fff',
+    shadowColor: '#333',
+    shadowOffset: {height: 2, width: 2},
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    paddingHorizontal: 5,
+    paddingVertical: 10,
+    minWidth: '50%',
+    borderRadius: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   buttonStyle: {
-    position: 'absolute',
-    right: 25,
-    top: 25,
+    height: 30,
+    width: 40,
+    borderRadius: 40,
+    backgroundColor: '#007bff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
