9ed7220ec55c63bd29ce846ae969ee2d443733ce
deepakverma4 <er.deepakverma4u@gmail.com>
2021-09-24 14:04:56 +0530

    chore: animated fab added

--- AnimationTest.js
+++ AnimationTest.js
@@ -20,215 +20,120 @@ export class AnimationTest extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      viewAnimation: new Animated.Value(0),
       buttonAnimation: new Animated.Value(0),
-      showView: true,
-      showButton: true,
-      color: '#333',
     };
+    this._open = true;
   }
 
-  toggleView = () => {
-    const toValue = this.state.showView ? 1 : 0;
-    Animated.spring(this.state.viewAnimation, {
-      toValue,
-      useNativeDriver: true,
-    }).start();
+  buttonPress = () => {
+    const toValue = this._open ? 1 : 0;
 
-    this.setState({
-      showView: !this.state.showView,
-    });
-  };
-
-  toggleButton = () => {
-    const toValue = this.state.showButton ? 1 : 0;
     Animated.timing(this.state.buttonAnimation, {
       toValue,
       duration: 300,
-      useNativeDriver: true,
+      useNativeDriver: false,
     }).start();
 
-    this.setState({
-      showButton: !this.state.showButton,
-    });
+    this._open = !this._open;
   };
 
-  colorChange = (text) => {
-    this.setState({
-      color: text,
-    });
-  };
-
-  closeButton = () => {
-    Animated.sequence([
-      Animated.timing(this.state.buttonAnimation, {
-        toValue: 0,
-        duration: 300,
-        useNativeDriver: true,
-      }),
-      Animated.delay(10),
-      Animated.spring(this.state.viewAnimation, {
-        toValue: 0,
-        useNativeDriver: true,
-      }),
-    ]).start(() => {
-      this.setState({
-        showView: false,
-        showButton: false,
-      });
-    });
-  };
   render() {
-    const viewInterpolate = this.state.viewAnimation.interpolate({
-      inputRange: [0, 1],
-      outputRange: [0, -100],
-    });
-
-    const viewScaleInterpolate = this.state.viewAnimation.interpolate({
-      inputRange: [0, 0.5, 1],
-      outputRange: [0, 0, 1],
-    });
-
-    const animatedViewStyle = {
-      opacity: this.state.viewAnimation,
-      transform: [
-        {
-          translateY: viewInterpolate,
-        },
-        {scaleX: viewScaleInterpolate},
-      ],
-    };
-
-    const moveInterpolate = this.state.buttonAnimation.interpolate({
-      inputRange: [0, 1],
-      outputRange: [-50, 0],
-    });
-
-    const viewInterpolateOpacity = this.state.buttonAnimation.interpolate({
+    const interpolate = this.state.buttonAnimation.interpolate({
       inputRange: [0, 1],
       outputRange: [0, 1],
     });
-
-    const textInterpolateOpacity = this.state.buttonAnimation.interpolate({
-      inputRange: [0, 0.5, 1],
-      outputRange: [1, 1, 0],
-    });
-
-    const animatedTextStyle = {
-      opacity: textInterpolateOpacity,
-      transform: [{scale: textInterpolateOpacity}],
+    const button1Style = {
+      opacity: interpolate,
+      transform: [
+        {
+          translateY: this.state.buttonAnimation.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0, -70],
+          }),
+        },
+      ],
     };
 
-    const animatedTopView = {
+    const button2Style = {
+      opacity: interpolate,
       transform: [
-        {translateX: moveInterpolate},
-        {scaleY: this.state.buttonAnimation},
+        {
+          translateY: this.state.buttonAnimation.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0, -140],
+          }),
+        },
       ],
-      opacity: viewInterpolateOpacity,
+    };
+
+    const button3Style = {
+      opacity: interpolate,
+      transform: [
+        {
+          translateY: this.state.buttonAnimation.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0, -210],
+          }),
+        },
+      ],
+    };
+
+    const buttonTextStyle = {
+      opacity: interpolate,
+
+      right: this.state.buttonAnimation.interpolate({
+        inputRange: [0, 1],
+        outputRange: [0, 80],
+      }),
     };
 
     return (
       <SafeAreaView style={styles.container}>
-        <Animated.View style={[styles.viewStyle, animatedViewStyle]}>
-          <TouchableOpacity onPress={this.toggleButton}>
-            <View
-              style={{
-                height: 20,
-                width: 20,
-                backgroundColor: this.state.color,
-                borderRadius: 20,
-              }}
-            />
-          </TouchableOpacity>
-          <View
-            style={{
-              flexDirection: 'row',
-              alignItems: 'center',
-              justifyContent: 'space-around',
-
-              flex: 1,
-            }}>
-            <TouchableOpacity
-              style={{
-                alignItems: 'center',
-                justifyContent: 'center',
-                padding: 3,
-              }}>
-              <Animated.Text style={[{fontSize: 20}, animatedTextStyle]}>
-                A
-              </Animated.Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={{
-                alignItems: 'center',
-                justifyContent: 'center',
-                padding: 3,
-              }}>
-              <Animated.Text style={[{fontSize: 20}, animatedTextStyle]}>
-                B
-              </Animated.Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity
-              style={{
-                alignItems: 'center',
-                justifyContent: 'center',
-                padding: 3,
-              }}>
-              <Animated.Text style={[{fontSize: 20}, animatedTextStyle]}>
-                C
-              </Animated.Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity
-              style={{
-                alignItems: 'center',
-                justifyContent: 'center',
-                padding: 3,
-              }}>
-              <Animated.Text style={[{fontSize: 20}, animatedTextStyle]}>
-                D
-              </Animated.Text>
-            </TouchableOpacity>
-          </View>
-
+        <TouchableWithoutFeedback activeOpacity={0.6} onPress={() => {}}>
           <Animated.View
-            pointerEvents={this.state.showButton ? 'none' : 'auto'}
-            style={[
-              StyleSheet.absoluteFill,
-              {
-                flexDirection: 'row',
-                alignItems: 'center',
-                paddingLeft: 30,
-              },
-              animatedTopView,
-            ]}>
-            <AnimatedTextInput
-              ref={(input) => (this._input = input)}
-              style={{flex: 1}}
-              value={this.state.color}
-              onChangeText={this.colorChange}
-            />
-            <TouchableOpacity onPress={this.closeButton}>
-              <Animated.View style={[styles.buttonStyle]}>
-                <Text style={{color: '#fff', padding: 5}}>OK</Text>
-              </Animated.View>
-            </TouchableOpacity>
+            style={[styles.buttonStyle, styles.otherButton, button3Style]}>
+            <Animated.Text
+              style={[
+                {fontSize: 18, color: '#000', position: 'absolute', right: 20},
+                buttonTextStyle,
+              ]}>
+              3 text
+            </Animated.Text>
+            <Text style={[styles.otherTextStyle]}>3</Text>
           </Animated.View>
-        </Animated.View>
+        </TouchableWithoutFeedback>
+
+        <TouchableWithoutFeedback activeOpacity={0.6} onPress={() => {}}>
+          <Animated.View
+            style={[styles.buttonStyle, styles.otherButton, button2Style]}>
+            <Animated.Text
+              style={[
+                {fontSize: 18, color: '#000', position: 'absolute', right: 20},
+                buttonTextStyle,
+              ]}>
+              2 text
+            </Animated.Text>
+            <Text style={[styles.otherTextStyle]}>2</Text>
+          </Animated.View>
+        </TouchableWithoutFeedback>
+        <TouchableWithoutFeedback activeOpacity={0.6} onPress={() => {}}>
+          <Animated.View
+            style={[styles.buttonStyle, styles.otherButton, button1Style]}>
+            <Animated.Text
+              style={[
+                {fontSize: 18, color: '#000', position: 'absolute', right: 20},
+                buttonTextStyle,
+              ]}>
+              1 text
+            </Animated.Text>
+            <Text style={[styles.otherTextStyle]}>1</Text>
+          </Animated.View>
+        </TouchableWithoutFeedback>
         <TouchableOpacity
           activeOpacity={0.6}
-          onPress={this.toggleView}
-          style={{
-            borderRadius: 10,
-            borderWidth: 0.8,
-            borderColor: '#333',
-            paddingVertical: 5,
-            paddingHorizontal: 8,
-          }}>
-          <Text style={[styles.textStyle, {color: this.state.color}]}>
-            Toggle View
-          </Text>
+          onPress={this.buttonPress}
+          style={[styles.buttonStyle]}>
+          <Text style={[styles.textStyle]}>FAB</Text>
         </TouchableOpacity>
       </SafeAreaView>
     );
@@ -243,32 +148,33 @@ const styles = StyleSheet.create({
     // flexGrow: 1,
 
     backgroundColor: 'white',
+  },
+
+  buttonStyle: {
+    height: 60,
+    width: 60,
+    borderRadius: 60,
+    backgroundColor: '#007bff',
+    position: 'absolute',
+    right: 12,
+    bottom: 30,
     alignItems: 'center',
     justifyContent: 'center',
+    shadowOpacity: 0.2,
+    shadowOffset: {height: 2, width: 2},
+    shadowRadius: 3,
+    shadowColor: '#333',
+  },
+  otherButton: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
   },
   textStyle: {
     fontSize: 18,
-    padding: 5,
+    color: '#fff',
   },
-  viewStyle: {
-    backgroundColor: '#fff',
-    shadowColor: '#333',
-    shadowOffset: {height: 2, width: 2},
-    shadowOpacity: 0.2,
-    shadowRadius: 3,
-    paddingHorizontal: 5,
-    paddingVertical: 10,
-    minWidth: '50%',
-    borderRadius: 20,
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  buttonStyle: {
-    height: 30,
-    width: 40,
-    borderRadius: 40,
-    backgroundColor: '#007bff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  otherTextStyle: {
+    fontSize: 18,
+    color: '#000',
   },
 });
